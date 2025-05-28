20de2dfcf991dd14d88ba3eda9f6bf9dc28ba9d4
Anjali choudhary <anjalianjali06149@gmail.com>
2025-05-28 23:55:00 +0530

    Add files via upload

---
+++ question12.java
@@ -0,0 +1,39 @@
+public class question12{
+    public static void main(String[] args) {
+        int arr[] = {1,2,4,1,6,7,5};
+
+        int n =arr.length;
+        int count = 0;
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        int[] temp = new int[n];
+        for (i = 0; i < n; i++){
+            count = 1;
+            for (j = i + 1; j < n; j++){
+                if (arr[i] == arr[j]){
+                    count++;
+                    temp[j] = -1;
+                }
+
+            }
+            if (temp[i] != -1){
+                temp[i] = count;
+            }
+        }
+        System.out.println("the occurence of each element in the array is:");
+        for (i = 0; i < n; i++) {
+            if (temp[i] != -1){
+                System.out.println(arr[i] + " occurs " + temp[i]+ "time");
+            }
+        }
+        System.out.println("the unique elemts in the array are:");
+        for (i= 0; i < n; i++) {
+            if (temp[i] != -1) {
+                 System.out.println(arr[i] + " ");
+            }
+        }
+        System.out.println();
+    }
+
+}
