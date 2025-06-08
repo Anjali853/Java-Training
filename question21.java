e09fc510e6e428182a925bc5b2f9e31291d13d37
Anjali choudhary <anjalianjali06149@gmail.com>
2025-06-08 23:37:52 +0530

    Add files via upload

---
+++ question21.java
@@ -0,0 +1,12 @@
+ public class question21 {
+    public static void main(String[] args) {
+        String str = "Hello Java";
+        String reversed = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+
+        System.out.println("Reversed: " + reversed);
+    }
+}
