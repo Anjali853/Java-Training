8a7e37ce4850ed3cff1f78da1c02392de068286c
Anjali choudhary <anjalianjali06149@gmail.com>
2025-06-18 00:01:56 +0530

    Add files via upload

---
+++ question28.java
@@ -0,0 +1,31 @@
+public class question28 {
+    public static int myAtoi(String sc) {
+        if (sc == null || sc.length() == 0) return 0;
+
+        sc = sc.trim();
+        if (sc.length() == 0) return 0;
+
+        int sign = 1, i = 0;
+        long result = 0;
+
+        if (sc.charAt(i) == '+' || sc.charAt(i) == '-') {
+            sign = (sc.charAt(i) == '-') ? -1 : 1;
+            i++;
+        }
+        while (i < sc.length() && Character.isDigit(sc.charAt(i))) {
+            result = result * 10 + (sc.charAt(i) - '0');
+
+            if (sign * result > Integer.MAX_VALUE) return Integer.MAX_VALUE;
+            if (sign * result < Integer.MIN_VALUE) return Integer.MIN_VALUE;
+
+            i++;
+        }
+
+        return (int)(sign * result);
+    }
+
+    public static void main(String[] args) {
+        String input = "-24";
+        System.out.println("Converted Integer: " + myAtoi(input));
+    }
+}
