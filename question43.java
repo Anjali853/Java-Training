c80385c65efc3daabf02f87d8456014f038ce018
Anjali choudhary <anjalianjali06149@gmail.com>
2025-07-03 00:08:26 +0530

    Add files via upload

---
+++ question43.java
@@ -0,0 +1,27 @@
+public class question43 {
+    public static void main(String[] args) {
+    
+         int[][] matrix = {
+         {1, 2, 3, 4},
+         {5, 6, 7, 8},
+         {9, 10, 11, 12}
+      };
+      int rows = matrix.length;
+      int cols = matrix[0].length;
+
+      for(int i = 0; i < rows; i++){
+         if(i % 2 == 0){
+            for(int j = 0; j < cols; j++ ){
+            System.out.print(matrix[i][j] + " ");
+         }
+         
+         } else {
+            for(int j = cols - 1; j >= 0; j--){
+            System.out.print(matrix[i][j] + " ");
+         }
+      }
+      System.out.println();
+}
+    }
+   }
+      
